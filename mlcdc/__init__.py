9696cbdb9fad7541af150afd5c6ebba87928ed06
timothyas <timsmith204@utexas.edu>
2022-09-09 10:45:58 -0600

    kfoldfeeder ... taking full advantage of multiple inheritance...

--- mlcdc/__init__.py
+++ mlcdc/__init__.py
@@ -1,12 +1,14 @@
 
 from .gcmdataconverter import GCMDataConverter
 from .kerasfeeder import KerasFeeder, SurfaceFeeder
+from .kfoldfeeder import KFoldKerasFeeder, KFoldSurfaceFeeder
 from .plot import histoscatter
 
 __all__ = [
         "GCMDataConverter",
         "KerasFeeder",
         "SurfaceFeeder",
+        "kfoldfeeder",
         "load_data_fns",
         "plot",
         "utils",
